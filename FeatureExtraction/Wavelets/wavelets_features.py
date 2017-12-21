d7cf076e9e1cb986cea7122d3873fe48dd304f71
szympie <justpiech@gmail.com>
2017-12-21 14:17:22 +0100

    Docstrings added, removed error connected with supported wavelets

--- FeatureExtraction/Wavelets/wavelets_features.py
+++ FeatureExtraction/Wavelets/wavelets_features.py
@@ -3,28 +3,56 @@ import numpy as np
 from scipy import stats
 from skimage import color, io
 import pywt
+from data_types import GreyscaleImage
 
 
-def get_wavelet_features(image: np.ndarray, mother_wavelet: str) -> dict:
-    if mother_wavelet not in ["haar", "db", "sym", "coif", "bior", "rbio", "dmey", "gaus", "mexh", "morl", "cgau",
-                              "shan", "fbsp", "cmor"]:
-        raise ValueError("Given mother wavelet \"{}\" is not supported".format(mother_wavelet))
-    greyscale_image = color.rgb2grey(image)
-    coeffs = pywt.dwt2(greyscale_image, mother_wavelet)
+def get_wavelet_features(image: GreyscaleImage, mother_wavelet: str) -> dict:
+    """
+    Extracts wavelet features from an image in a greyscale. For each detail
+    coefficients matrices (cH, cV, cD) it extracts:
+    maximal value,
+    average value,
+    kurtosis,
+    skewness
+
+    :param image: Pixel values of the image from which you want to extract
+        features
+    :param mother_wavelet: Wavelet type to perform Discrete Wavelet Transform
+    :return: Dictionary with features extracted from an image.
+    """
+    if not isinstance(image, GreyscaleImage):
+        raise TypeError(
+            "Image should be in greyscale, use GreyscaleImage type")
+    coeffs = pywt.dwt2(image, mother_wavelet)
     cA, (cH, cV, cD) = coeffs
     features_dict = {}
     for detail_coefficients, name in zip([cH, cV, cD], ["cH", "cV", "cD"]):
-        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(detail_coefficients)
+        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(
+            detail_coefficients)
         features = dict(
-            {name + "_max": max, name + "_avg": avg, name + "_kurtosis": kurtosis, name + "_skewness": skewness})
+            {mother_wavelet + "_" + name + "_max": max,
+             mother_wavelet + "_" + name + "_avg": avg,
+             mother_wavelet + "_" + name + "_kurtosis": kurtosis,
+             mother_wavelet + "_" + name + "_skewness": skewness})
         features_dict = {**features_dict, **features}
     return features_dict
 
 
-def get_features_for_detail_coefficients(detail_coefficients: np.ndarray) -> dict:
+def get_features_for_detail_coefficients(
+        detail_coefficients: np.ndarray) -> dict:
+    """
+    Extracts wavelet coefficients from a single detail coefficients matrix.
+    :param detail_coefficients: Single detail coefficients matrix taken from
+        Discrete Wavelet Transform
+    :return: Values of the features: max, avg, kurtosis, skewness
+    """
     detail_coefficients_flattened = detail_coefficients.flatten()
     max = detail_coefficients_flattened.max()
     avg = detail_coefficients_flattened.mean()
     kurtosis = stats.kurtosis(detail_coefficients_flattened)
     skewness = stats.skew(detail_coefficients_flattened)
     return max, avg, kurtosis, skewness
+
+image = io.imread("/Users/apple/PycharmProjects/BACH/Dataset/Training_data/Benign/norm/normt2.tif")
+image = GreyscaleImage(image)
+print(get_wavelet_features(image, "db1"))
--- tests/test_wavelets_features.py
+++ tests/test_wavelets_features.py
@@ -1,7 +1,9 @@
 import unittest
 import numpy as np
-from FeatureExtraction.Wavelets.wavelets_features import get_wavelet_features, get_features_for_detail_coefficients
+from FeatureExtraction.Wavelets.wavelets_features import get_wavelet_features, \
+    get_features_for_detail_coefficients
 from data_types import GreyscaleImage
+
 
 class TestGettingWaveletsFeatures(unittest.TestCase):
     def setUp(self):
@@ -9,7 +11,8 @@ class TestGettingWaveletsFeatures(unittest.TestCase):
         self.image = GreyscaleImage(pixels)
 
     def test_that_wrong_mother_wavelet_raise_exception(self):
-        self.assertRaises(ValueError, get_wavelet_features, self.image, "wrong_wavelet")
+        self.assertRaises(ValueError, get_wavelet_features, self.image,
+                          "wrong_wavelet")
 
 
 class TestGettingDetailCoefficionsFeatures(unittest.TestCase):
@@ -18,17 +21,21 @@ class TestGettingDetailCoefficionsFeatures(unittest.TestCase):
         self.image.reshape((20, 5))
 
     def test_that_returns_correct_max(self):
-        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(self.image)
+        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(
+            self.image)
         self.assertEqual(max, 99)
 
     def test_that_returns_correct_avg(self):
-        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(self.image)
+        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(
+            self.image)
         self.assertEqual(avg, 49.5)
 
     def test_that_returns_correct_kurtosis(self):
-        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(self.image)
+        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(
+            self.image)
         self.assertAlmostEqual(kurtosis, -1.2002400240024003, 9)
 
     def test_that_returns_correct_skewness(self):
-        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(self.image)
+        max, avg, kurtosis, skewness = get_features_for_detail_coefficients(
+            self.image)
         self.assertEqual(skewness, 0)
